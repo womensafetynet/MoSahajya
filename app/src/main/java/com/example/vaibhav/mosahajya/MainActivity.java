436c04355b7bb9d482bebcab72abdd13bd21a97b
vaibhav1seth <vaibhavk774@gmail.com>
2019-02-15 16:11:47 +0530

    ntoification enabled

--- app/src/main/java/com/example/vaibhav/mosahajya/MainActivity.java
+++ app/src/main/java/com/example/vaibhav/mosahajya/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.vaibhav.mosahajya;
 
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+
+                        if (!task.isSuccessful()){
+
+                            Log.w("getinstacefailed",task.getException());
+                            return;
+                        }
+
+                        String token = task.getResult().getToken();
+
+                        String mssg = "helpme !!";
+
+                        Log.d("instacetoken",mssg);
+
+                        Toast.makeText(getApplicationContext(),"mssg",Toast.LENGTH_LONG).show();
+
+                    }
+                });
     }
+
+
 }
---
+++ app/src/main/java/com/example/vaibhav/mosahajya/service/MyfirebaseInstanceService.java
@@ -0,0 +1,80 @@
+package com.example.vaibhav.mosahajya.service;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.example.vaibhav.mosahajya.R;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Random;
+
+public class MyfirebaseInstanceService extends FirebaseMessagingService {
+
+
+
+
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        //String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+
+        Log.d("Token Firebase",s);
+
+
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    private void showNotification(String title, String body) {
+
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(getApplicationContext().NOTIFICATION_SERVICE);
+
+        String NOTIFICATION_CHANNEL_ID ="com.example.vaibhav.mosahajya.test";
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("HELP ME");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+        }
+
+        //NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+
+        NotificationCompat.Builder  notificationBuilder = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+        notificationManager.notify(new Random().nextInt(),notificationBuilder.build());
+
+
+    }
+}
